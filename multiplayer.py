485d2a214424251ef68537ae8c688df5b2766933
Camilo <camilomoreira91@gmail.com>
2016-05-15 11:43:52 -0300

    Move execucao do servidor para server_handler.py. Adiciona esboco de threads para enviar para e receber dados do servidor. Adiciona exemplo de execucao de comunicacao.

--- multiplayer.py
+++ multiplayer.py
@@ -1,10 +1,45 @@
-from server_handler import *
+import requests
+import threading
+import time
+from constants import SERVER_ADRESS, ROUTE_PARTIDAS
 
-SERVER_PORT = 8000
+def thread_get_data(player,partida,lista):
 
-def thread_init_server():
-    server_address = ('', SERVER_PORT)
-    server = BaseHTTPServer.HTTPServer(server_address, MineSweeperServer)
-    sa = server.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
-    server.serve_forever()
+    if lista is None:
+        raise RuntimeError('Lista compartilhada nao pode ser nula')
+
+    ROUTE = ROUTE_PARTIDAS + '/' + str(partida) + '/' + str(player)
+    while True:
+
+        if len(lista) > 0 and type(lista[0]) is bool:
+            break
+
+        try:
+            result = requests.get(ROUTE).content
+            params = {}
+            if result is not None and len(result) > 0:
+                for par in result.split('&'):
+                    par = str(par).split('=')
+                    params[str(par[0])] = str(par[1])
+                requests.delete(ROUTE)
+                if str(params['player']) != str(player):
+                    print params
+                    lista.append((params['x'], params['y'], params['action']))
+        except Exception as ex:
+            print ex
+            break
+
+def thread_send_data(x,y,player,partida,action):
+    ROUTE = ROUTE_PARTIDAS + '/' + str(partida)
+    data = {'x':x,'y':y,'player':player,'action':action}
+    try:
+        for i in range(2):
+            requests.post(ROUTE + '/' + str(i), data)
+    except:
+        print "Nao foi possivel salvar clique"
+
+if __name__ == '__main__':
+    thread_send_data(10, 10, 1, 'test3', 1)
+    thread_send_data(10, 11, 1, 'test3', 1)
+    thread_send_data(10, 12, 1, 'test3', 1)
+    thread_get_data(0, 'test3', [])

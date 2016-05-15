eaed902580f91009cdb6e6ecd05be59dea0e3333
Camilo <camilomoreira91@gmail.com>
2016-05-15 11:40:27 -0300

    Adiciona lista de requisicoes. Deste modo, GET sempre pega a requisicao mais antiga, DELETE sempre remove a requisicao mais antiga e POST insere uma requisicao no fim da lista. Sobrescreve metodo log_message() para evitar geracao de saida desnecessaria. Adiciona main() do servidor.

--- server_handler.py
+++ server_handler.py
@@ -1,14 +1,23 @@
-import BaseHTTPServer
-import urlparse
-import json
-
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from constants import SERVER_PORT
+import datetime
 server_data = {}
 
-class MineSweeperServer(BaseHTTPServer.BaseHTTPRequestHandler):
+def clean_path(path):
+    path = path.strip().split('/')
+    # Remove string vazias
+    path = [x for x in path if x]
+    return path
+
+class MineSweeperServer(BaseHTTPRequestHandler):
+
+    # Remova esse metodo para ver todas as requests chegando
+    def log_message(self, format, *args):
+        pass
 
     def do_GET(self):
-        print self.requestline
-        path = self.path.strip().split('/')[1:]
+        # print self.requestline
+        path = clean_path(self.path)
 
         ultimo_dict = server_data
         while len(path) > 0:
@@ -20,19 +29,25 @@ class MineSweeperServer(BaseHTTPServer.BaseHTTPRequestHandler):
 
             path = path[1:]
 
+        try:
+            chave_mais_antiga = sorted(ultimo_dict.keys())[0]
+            ultimo_dict = ultimo_dict[chave_mais_antiga]
+        except:
+            pass # Dict esta vazio
+
         response = ''
         for key, val in ultimo_dict.items():
             response += '&' + key + '=' + val
         response = response[1:]
-        print response
+        # print response
 
         self.send_response(200)
         self.end_headers()
         self.wfile.write(response)
 
     def do_POST(self):
-        print self.requestline
-        path = self.path.strip().split('/')[1:]
+        # print self.requestline
+        path = clean_path(self.path)
 
         ultimo_dict = server_data
         while len(path) > 0:
@@ -47,22 +62,27 @@ class MineSweeperServer(BaseHTTPServer.BaseHTTPRequestHandler):
         content_length = int(self.headers.getheader('content-length', 0))
         POST_data = self.rfile.read(content_length)
 
+        instante = datetime.datetime.now()
+        chave_instante = str(instante).replace('.', ':')
+        ultimo_dict[chave_instante] = {}
+        ultimo_dict = ultimo_dict[chave_instante]
+
         for par in str(POST_data).split('&'):
             par = str(par).split('=')
             ultimo_dict[str(par[0])] = str(par[1])
 
-        print server_data
+        # print server_data
 
         self.send_response(200)
         self.end_headers()
         self.wfile.write('OK')
 
     def do_DELETE(self):
-        print self.requestline
-        path = self.path.strip().split('/')[1:]
+        # print self.requestline
+        path = clean_path(self.path)
 
         ultimo_dict = server_data
-        while len(path) > 1:
+        while len(path) > 0:
             if path[0] in ultimo_dict:
                 ultimo_dict = ultimo_dict[path[0]]
             else:
@@ -71,10 +91,20 @@ class MineSweeperServer(BaseHTTPServer.BaseHTTPRequestHandler):
 
             path = path[1:]
 
-        print server_data
-        del ultimo_dict[path[0]]
-        print server_data
+        try:
+            chave_mais_antiga = sorted(ultimo_dict.keys())[0]
+
+            del ultimo_dict[chave_mais_antiga]
+        except:
+            pass # Dict esta vazio
 
         self.send_response(200)
         self.end_headers()
         self.wfile.write('')
+
+if __name__ == '__main__':
+    server_address = ('', SERVER_PORT)
+    server = HTTPServer(server_address, MineSweeperServer)
+    sa = server.socket.getsockname()
+    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    server.serve_forever()

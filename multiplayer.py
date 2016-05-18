e96f11d2a10410c6cdc09536c4fbe08e804e1d95
Camilo <camilomoreira91@gmail.com>
2016-05-18 20:57:24 -0300

    Adiciona funcoes para comunicacao do servidor para os novos servicos. Envia excecoes para quem chamou o metodo ao inves de tentar tratar

--- multiplayer.py
+++ multiplayer.py
@@ -4,7 +4,7 @@ from constants import *
 from classes import GameController
 
 
-def get_tauleiro_from_server(partida):
+def get_tabuleiro_from_server(partida):
 
     ROUTE = ROUTE_TABULEIROS + '/' + str(partida)
     tabuleiro = ''
@@ -17,10 +17,62 @@ def get_tauleiro_from_server(partida):
                 par = str(par).split('=')
                 if str(par[0]) == 'tabuleiro':
                     tabuleiro = str(par[1])
-    except Exception as ex:
-        print ex
+    except:
+        raise RuntimeError('Nao foi possivel conectar no servidor')
 
     return tabuleiro
+
+def create_new_tabuleiro_on_server(partida):
+
+    # Tabuleiro sempre muda quando pelo menos uma das variaveis muda
+    GameController.bombs += 1
+    get_tabuleiro_from_server(partida)
+
+    GameController.bombs -= 1
+    try:
+        return get_tabuleiro_from_server(partida)
+    except:
+        raise RuntimeError('Nao foi possivel conectar no servidor')
+
+def get_player_ID(player,partida):
+    ROUTE = ROUTE_JOGADORES + '/' + str(partida)
+    dados = {'player':player}
+
+    try:
+        ID = requests.post(ROUTE, player).content
+        return int(ID)
+    except:
+        raise RuntimeError('Nao foi possivel conectar no servidor')
+
+def set_up_new_match(partida):
+    ROUTE = ROUTE_PARTIDAS + '/' + str(partida)
+
+    try:
+        requests.post(ROUTE, partida)
+    except:
+        raise RuntimeError('Nao foi possivel conectar no servidor')
+
+def start_match(partida):
+    ROUTE = ROUTE_PARTIDAS + '/' + str(partida)
+
+    try:
+        requests.delete(ROUTE, partida)
+    except:
+        raise RuntimeError('Nao foi possivel conectar no servidor')
+
+def wait_match_to_begin(partida):
+    ROUTE = ROUTE_PARTIDAS + '/' + str(partida)
+
+    while True:
+        try:
+            response = requests.get(ROUTE, partida).content
+            if len(response) == 0:
+                break
+        except:
+            raise RuntimeError('Nao foi possivel conectar no servidor')
+
+        time.sleep(0.1)
+
 
 def thread_get_data(player,partida,lista):
 
@@ -45,9 +97,8 @@ def thread_get_data(player,partida,lista):
                 if str(params['player']) != str(player):
                     print params
                     lista.append((params['x'], params['y'], params['action']))
-        except Exception as ex:
-            print ex
-            break
+        except:
+            raise RuntimeError('Nao foi possivel conectar no servidor')
 
         time.sleep(0.1)
 
@@ -58,7 +109,7 @@ def thread_send_data(x,y,player,partida,action):
         for i in range(5):
             requests.post(ROUTE + '/' + str(i), data)
     except:
-        print "Nao foi possivel salvar clique"
+        raise RuntimeError('Nao foi possivel conectar no servidor')
 
 if __name__ == '__main__':
     thread_send_data(10, 10, 1, 'test3', 1)
